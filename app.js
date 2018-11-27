5bcdfe00f8d6e96ebca7e02b9db0c45ec309e1c1
sekenikola <you@example.com>
2018-11-27 11:07:18 +0100

    fix small bugs

---
+++ app.js
@@ -0,0 +1,6 @@
+var loader = document.getElementById('loader');
+
+window.addEventListener('load', function(){
+    loader.setAttribute("style","width: 100x; height:100px; borderRadius:50%; visibility:hidden");
+
+});
